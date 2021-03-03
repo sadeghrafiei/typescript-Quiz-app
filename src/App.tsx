ebb862c1b0083459676f8401d0dc1e4e6baf80f8
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-03 17:30:33 +0330

    add QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-function App() {
+import React , {useState} from 'react';
+import QuestionCard from './components/QuestionCard'
+
+const TOTAL_QUESTIONS = 10 ;
+
+const App = () => {
+  const [loading , setLoading] = useState(false);
+  const [questions , setQuestions] = useState([]);
+  const [number , setNumber] = useState(0);
+  const [userAnswers , setUserAnswers] = useState([])
+  const [acore , setScore] = useState(0);
+  const [gameOver , setGameOver] = useState(true);
+
+
+  const startTrivia = async () => {
+
+  }
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
-    <div className="App">
-      Quiz
+    <div>
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Question ...</p>
+      <QuestionCard 
+        questionNr={number + 1}
+        totalQuestion={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answer}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
   );
 }
