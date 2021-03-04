720344850f30b2db1ff93c8e5d4790c89fb99277
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-04 12:15:49 +0330

    complete question card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,37 +1,45 @@
 import React from 'react';
+// Types
+import { AnswerObject } from '../App';
+// Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 type Props = {
-    question: string;
-    answers: string[];
-    callback: any;
-    userAnswer: any;
-    questionNr: number;
-    totalQuestion: number;
-}
+  question: string;
+  answers: string[];
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
+  questionNr: number;
+  totalQuestions: number;
+};
 
 const QuestionCard: React.FC<Props> = ({
-    question,
-    answers,
-    callback,
-    questionNr,
-    userAnswer,
-    totalQuestion
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
 }) => (
+  <Wrapper>
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-        <p className="number">
-            Question: {questionNr} / {totalQuestion}
-        </p>
-        <p dangerouslySetInnerHTML={{ __html: question }} />
-        <div>
-            {answers.map(answer => (
-                <div>
-                    <button disabled={userAnswer} onClick={callback}>
-                        <span dangerouslySetInnerHTML={{ __html: answer }} />
-                    </button>
-                </div>
-            ))}
-        </div>
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
     </div>
+  </Wrapper>
 );
 
 export default QuestionCard;
